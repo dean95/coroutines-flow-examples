0af372c6a4b25d23508fe979eefc178386a3f039
Dean Djermanovic <dean.djermanovic@five.agency>
2020-12-28 15:42:10 +0100

    Add Example 4

---
+++ src/main/kotlin/Example4.kt
@@ -0,0 +1,28 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Implement a toggle mechanism. Initially we want to return false.
+ * Every time [source] emits, we want to negate the previous value.
+ *
+ * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
+ */
+
+private fun solve(source: Flow<Unit>): Flow<Boolean> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+    val source = flow {
+        repeat(10) {
+            emit(Unit)
+            delay(300)
+        }
+    }
+
+    solve(source)
+            .collect {
+                println(it)
+            }
+}
