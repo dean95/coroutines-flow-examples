7059e1e6cbbde8b3db387667941f61426c0d18c0
Dean Djermanovic <dean.djermanovic@five.agency>
2020-12-29 11:26:20 +0100

    Add Example 5

---
+++ src/main/kotlin/Example5.kt
@@ -0,0 +1,23 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Sum up the latest values of [first] and [second] whenever one of the Flows emits a new value.
+ *
+ * Use case: Two input fields in a calculator that need to be summed up and the result should be updated every time one of the inputs change.
+ */
+
+private fun solve(first: Flow<Int>, second: Flow<Int>): Flow<Int> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+    val first = (1..10).asFlow().onEach { delay(200) }
+    val second = (1..10).asFlow().onEach { delay(300) }
+
+    solve(first, second)
+        .collect {
+            println(it)
+        }
+}
