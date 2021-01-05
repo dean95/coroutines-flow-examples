aa11f8097fa37a8c3871680db9acb85c4fddd44a
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-05 14:21:49 +0100

    Solved

--- src/main/kotlin/Example3.kt
+++ src/main/kotlin/Example3.kt
@@ -1,6 +1,7 @@
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.runBlocking
 
 /**
@@ -9,13 +10,11 @@ import kotlinx.coroutines.runBlocking
  * Use case: You want to filter certain items out.
  */
 
-private fun solve(source: Flow<Int>): Flow<Int> {
-    TODO()
-}
+private fun solve(source: Flow<Int>): Flow<Int> = source.filter { it % 2 == 0 }
 
 private fun main() = runBlocking {
     solve((1..10).asFlow())
-            .collect {
-                println(it)
-            }
+        .collect {
+            println(it)
+        }
 }
