f436000858281e9c16a71ea4b054aa815612fbf5
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-09 16:50:51 +0100

    Add example 9

---
+++ src/main/kotlin/Example9.kt
@@ -0,0 +1,37 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+/**
+ * As long as the [trigger] Flow does not emit an item, keep the [main] Flow alive.
+ *
+ * Use case: Cancel a Flow when something has happened. For instance, stop polling when the user has been logged out.
+ */
+
+private fun solve(main: Flow<Unit>, trigger: Flow<Unit>): Flow<Unit> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+
+    val main = flow {
+        while (true) {
+            emit(Unit)
+            delay(500)
+        }
+    }
+
+    val trigger = flowOf(Unit)
+        .onEach {
+            delay(3000)
+            println("Trigger emitted")
+        }
+
+    try {
+        solve(main, trigger)
+            .collect {
+                println(it)
+            }
+    } catch (e: CancellationException) {
+        println("Collection cancelled")
+    }
+}
