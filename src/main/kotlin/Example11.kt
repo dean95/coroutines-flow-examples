11130dc1a9a2ced82e3f84c5acc3f6f614344aa1
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-16 09:37:53 +0100

    Add Example 11

---
+++ src/main/kotlin/Example11.kt
@@ -0,0 +1,31 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Let the first emission of the [source] within a time window of 300ms travel downstream but don't emit
+ * any other events until the next time window.
+ *
+ * Use case: Handle the click of a button right away but prevent double clicking by not handling multiple
+ * click events within a given time window.
+ */
+
+private fun solve(source: Flow<Unit>): Flow<Unit> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+    val startTime = System.currentTimeMillis()
+    val source = flow {
+        repeat(20) {
+            emit(Unit)
+            delay(100)
+        }
+    }
+
+    solve(source).collect {
+        println("Collecting: ${System.currentTimeMillis() - startTime}")
+    }
+}
