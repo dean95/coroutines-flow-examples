6aec41816de04b74a6ac5bb66e126683b3d17118
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-19 14:39:48 +0100

    Solve

--- src/main/kotlin/Example7.kt
+++ src/main/kotlin/Example7.kt
@@ -1,5 +1,6 @@
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
+import operators.distinct
 
 /**
  * When the [source] emits the same value multiple times, only allow the first value to travel downstream.
@@ -7,9 +8,7 @@ import kotlinx.coroutines.runBlocking
  * Use case: You never want to show the same value twice.
  */
 
-private fun solve(source: Flow<Int>): Flow<Int> {
-    TODO()
-}
+private fun solve(source: Flow<Int>): Flow<Int> = source.distinct()
 
 private fun main() = runBlocking {
     val source = flowOf(1, 2, 3, 3, 4, 5, 5, 6, 2, 7, 2, 1, 8)
