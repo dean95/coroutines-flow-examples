59393b074732aa36ac922ae4b3095bda86d711f7
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-19 09:05:10 +0100

    Fix Example 8

--- src/main/kotlin/Example8.kt
+++ src/main/kotlin/Example8.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.runBlocking
 /**
  * Delay the entire [source] by 200ms. This includes subscribing, emissions and terminal events.
  *
- * Use case: Make an Observable "lazy" for some time. For instance, when wanting to postpone some UI action.
+ * Use case: Make a Flow "lazy" for some time. For instance, when wanting to postpone some UI action.
  */
 
 private fun solve(source: Flow<Unit>): Flow<Unit> {
