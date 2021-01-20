32ad8725ca1637253b49e1444aff81aac6d5a91e
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-20 17:48:40 +0100

    Add Example 13

---
+++ src/main/kotlin/Example13.kt
@@ -0,0 +1,21 @@
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+/**
+ * When the [source] emits the same value as it did last time, don't allow it to travel downstream.
+ *
+ * Use case: You only want to observe changes of a value but don't care if the same value has been emitted consecutively.
+ */
+private fun solve(source: Flow<Int>): Flow<Int> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+    val source = flowOf(1, 2, 3, 4, 4, 5, 6, 7, 7, 7, 8, 9)
+    solve(source)
+        .collect {
+            println(it)
+        }
+}
