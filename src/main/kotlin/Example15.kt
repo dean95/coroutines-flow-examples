113155847beb5b48cd2e8245861d380668cbcd45
Dean Djermanovic <dean.djermanovic@five.agency>
2021-01-23 10:30:53 +0100

    Add Example 15

---
+++ src/main/kotlin/Example15.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Concatenate the [first] Flow with the [second] Flow, one after the other, without interleaving them.
+ *
+ * Use case: You have two sources of your data (cache & network request). You want to collect
+ * both of them and keep the emission order.
+ */
+private fun solve(first: Flow<Int>, second: Flow<Int>): Flow<Int> {
+    TODO()
+}
+
+private fun main() = runBlocking {
+    solve(cache(), network())
+        .collect {
+            println(it)
+        }
+}
+
+private fun network() = (1..10).asFlow().onEach { delay(300) }
+
+private fun cache() = (30..50).asFlow().onEach { delay(100) }
