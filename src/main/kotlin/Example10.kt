db881b0e54a303c4042832b3d212566ef4960e95
Dean Djermanovic <dean.djermanovic@five.agency>
2021-02-08 17:08:47 +0100

    Solve

--- src/main/kotlin/Example10.kt
+++ src/main/kotlin/Example10.kt
@@ -2,13 +2,14 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 /**
- * Use the [first] Flow and flatten it with the results of the [function] that returns an Flow.
+ * Use the [first] Flow and flatten it with the results of the [function] that returns a Flow.
  *
  * Use case: Get some user data and perform a network request with the user data and have both data accessible afterwards.
  */
-private fun solve(first: Flow<Int>, function: (Int) -> Flow<String>): Flow<Pair<Int, String>> {
-    TODO()
-}
+private fun solve(first: Flow<Int>, function: (Int) -> Flow<String>): Flow<Pair<Int, String>> =
+    first.flatMapLatest { firstValue ->
+        function(firstValue).map { secondValue -> firstValue to secondValue }
+    }
 
 private fun main() = runBlocking {
     solve(getUserId(), ::getUsername)
