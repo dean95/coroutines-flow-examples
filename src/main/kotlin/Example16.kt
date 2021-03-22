b83aae1bfa8cb7b487a2500cc496159f3e0835f5
Dean Djermanovic <dean.djermanovic@five.agency>
2021-03-22 19:30:34 +0100

    Solve

--- src/main/kotlin/Example16.kt
+++ src/main/kotlin/Example16.kt
@@ -2,6 +2,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.runBlocking
 
 /**
@@ -12,8 +13,8 @@ import kotlinx.coroutines.runBlocking
  * Use case: The [source] Flow is a search field and you want to issue a network request while disposing the
  * old requests in case the user has typed something new.
  */
-fun solve(source: Flow<String>, function: suspend (String) -> Int): Flow<Int> {
-    TODO()
+fun solve(source: Flow<String>, function: suspend (String) -> Int): Flow<Int> = source.mapLatest {
+    function(it)
 }
 
 private fun main() = runBlocking {
