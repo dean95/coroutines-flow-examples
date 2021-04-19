a2bbbc81bb36cb7321a21d9a4da838a9e7afc658
Dean Djermanovic <dean.djermanovic@five.agency>
2021-04-19 17:38:20 +0200

    Solve

--- src/main/kotlin/Example21.kt
+++ src/main/kotlin/Example21.kt
@@ -1,6 +1,7 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.runBlocking
 
@@ -9,9 +10,7 @@ import kotlinx.coroutines.runBlocking
  *
  * Use case: Sometimes you can't do everything reactively and need to break out of it.
  */
-private fun solve(source: Flow<Int>): Int {
-    TODO()
-}
+private suspend fun solve(source: Flow<Int>): Int = source.first()
 
 private fun main() = runBlocking {
     val source = (1..10).asFlow().onEach { delay(500) }
